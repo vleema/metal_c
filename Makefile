db19d5c2ba3e852f9d0d22d5bea38e9d2d3bfe01
Vinicius Lima <vldsampaio@pm.me>
2025-06-09 14:21:44 -0300

    Adicionar progresso da última aula 06/06/2025

--- Makefile
+++ Makefile
@@ -1,8 +1,8 @@
 kernel: entry.s main.c helper.c helper.h linker.ld
-	gcc -O3 -ffreestanding -o kernel entry.s main.c helper.c -nostdlib -m32 -g -T linker.ld -no-pie -mgeneral-regs-only
+	gcc -O3 -ffreestanding -o kernel entry.s main.c helper.c -nostdlib -m32 -g -T linker.ld -no-pie -mgeneral-regs-only -fno-stack-protector
 
 run: kernel
-	qemu-system-i386 -kernel ./kernel -nographic -s -no-reboot
+	qemu-system-i386 -kernel ./kernel -nographic -s -no-reboot -d int
 
 clean:
 	rm -rf kernel
--- main.c
+++ main.c
@@ -1,7 +1,30 @@
 #include "helper.h"
+#include <stdint.h>
 
-int main()
+uint32_t root[1024]
+    __attribute__((aligned(4096))) = {0x83, 0x83};
+
+int global = 10;
+
+int main() 
 {
-	puts("\nOlá mundo!\n");
-	for(;;);
+  puts("\nOlá mundo!\n");
+
+  int* ptr1 = &global;
+  int* ptr2 = (int*)((char*)&global + 4096 * 1024);
+
+  set_cr3((uint32_t)&root[0]);
+  set_cr4(get_cr4() | (1 << 4));
+  set_cr0(get_cr0() | (1 << 31));
+
+  *ptr2 = 20;
+
+  puts("Paginação Habilitada!\n");
+  puts("Valor de ptr1: ");
+  put_hex(*ptr1);
+  puts("\nValor de ptr2: ");
+  put_hex(*ptr2);
+  puts("\n");
+
+  for (;;);
 }
